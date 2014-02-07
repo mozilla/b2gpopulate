a1452acfbfa36c2e2b521c0f826378b1f8621e07
Jonathan Griffin <jgriffin@mozilla.com>
2014-02-07 09:46:23 -0800

    Bug 969158 - Bump b2gpopulate to 0.15, r=rwood

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 import os
 from setuptools import setup
 
-version = '0.14'
+version = '0.15'
 
 # get documentation from the README
 try:
@@ -15,7 +15,7 @@ except (OSError, IOError):
     description = ''
 
 # dependencies
-deps = ['gaiatest==0.21.4',
+deps = ['gaiatest==0.21.5',
         'mozdevice>=0.30',
         'mozlog>=1.4',
         'mutagen']
