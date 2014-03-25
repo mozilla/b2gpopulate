278e70154ecddce2a2eb5a86d39bb773c28b81a0
Dave Hunt <dave.hunt@gmail.com>
2014-03-25 18:10:47 +0000

    Bug 987881 - Bump gaiatest dependency to 0.21.9 r=jgriffin

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except (OSError, IOError):
     description = ''
 
 # dependencies
-deps = ['gaiatest==0.21.8',
+deps = ['gaiatest==0.21.9',
         'mozdevice>=0.30',
         'mozlog>=1.4',
         'mutagen']
